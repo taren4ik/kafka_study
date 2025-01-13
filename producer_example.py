271da4cb0c37b420cee8cf5190429b28a33c0d80
Dmitry Permyakov <taren4ik@mail.ru>
2025-01-13 23:37:12 +1000

    Создан producer

---
+++ producer_example.py
@@ -0,0 +1,12 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:dumps(x).encode('utf-8'))
+
+if __name__ == '__main__':
+    for e in range(5):
+        data = {'number': e}
+        producer.send('first_kafka_topic', value=data)
+        sleep(5)
