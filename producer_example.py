a85972bcbf5682e789428dc144d5bed92df7c85b
Dmitry Permyakov <taren4ik@mail.ru>
2025-01-15 23:21:41 +1000

    Создан consumer

--- producer_example.py
+++ producer_example.py
@@ -2,11 +2,16 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x:dumps(x).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=['host.docker.internal:9092'],
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 if __name__ == '__main__':
     for e in range(5):
         data = {'number': e}
-        producer.send('first_kafka_topic', value=data)
+        try:
+            producer.send('first_kafka_topic', value=data)
+            print(f'Message {e} send successfully')
+        except Exception as er:
+            print(f'Error send message {er}: {str(e)}')
         sleep(5)
+    producer.flush()
